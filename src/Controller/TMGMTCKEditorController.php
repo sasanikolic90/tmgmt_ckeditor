4a7389681dd7644fd272b6051a22f7199f7af2f0
sasanikolic <nikolic.sasa09@gmail.com>
2016-06-17 14:30:41 +0200

    Moved the segments in to .install, added comments.

--- src/Controller/TMGMTCKEditorController.php
+++ src/Controller/TMGMTCKEditorController.php
@@ -22,30 +22,13 @@ class TMGMTCKEditorController extends ControllerBase {
    *   Returns data in a json.
    */
   public function get(Request $request) {
-    $params = array();
     $content = $request->query->get('segment');
+    $json_segment = array();
     if (!empty($content)) {
       /**
        * @var \Drupal\tmgmt_memory\MemoryManager $memory_manager
        */
-      $memory_manager = \Drupal::service('tmgmt_memory.memory_manager');
-      $first_segment_text = 'This is the first segment.';
-      $second_segment_text = 'This is the second segment.';
-      $third_segment_text = 'This is the third segment.';
-
-      $first_segment_translated = 'This is the first translated segment';
-      $second_segment_translated = 'This is the second translated segment';
-      $third_segment_translated = 'This is the third translated segment';
-
-      $memory_manager->addSegment('en', $first_segment_text);
-      $memory_manager->addSegment('en', $second_segment_text);
-      $memory_manager->addSegment('en', $third_segment_text);
-
-      $memory_manager->addSegmentTranslation('en', $first_segment_text, 'de', $first_segment_translated);
-      $memory_manager->addSegmentTranslation('en', $second_segment_text, 'de', $second_segment_translated);
-      $memory_manager->addSegmentTranslation('en', $third_segment_text, 'de', $third_segment_translated);
       $translated_segment = $memory_manager->getSegmentTranslation('en', $content, 'de');
-
       $json_segment = array("translatedSegment" => $translated_segment->getTarget()->getStrippedData());
     }
     return new JsonResponse($json_segment);
